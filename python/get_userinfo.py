4a4bedd15a26f228a9631571c4382cebb054597c
Lucas Nguyen <lucasoAlt@outlook.com>
2022-08-28 12:26:34 +0200

    make getuserinfo api

---
+++ python/get_userinfo.py
@@ -0,0 +1,12 @@
+from js import XMLHttpRequest
+from js import console
+
+def get_userinfo(user):
+    request = XMLHttpRequest.new()
+    request.open("GET", f"https://api.github.com/users/{user}", False)
+    request.send(None)
+    output = str(request.response)
+
+    console.log(f"API Call run: {output}")
+
+    return output
